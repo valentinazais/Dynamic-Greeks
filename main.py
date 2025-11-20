bbd77df95ec3de1cf072a865dd7e9ea2c19ad3c6
Valentin Azaïs <100594639+valentinazais@users.noreply.github.com>
2025-11-20 11:21:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def black_scholes_option_price_and_greeks(S, K, T, r, q, sigma, option_type='cal
     }
 
 # Streamlit app
-st.title("Black-Scholes Option Strategy Dashboard (with Dividend Yield)")
+st.title("Black-Scholes Option Strategy Dashboard")
 
 # Initialize session state for parameters
 if 'S' not in st.session_state:
